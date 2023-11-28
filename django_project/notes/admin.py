925809fb1e1a75c22257bb82bd29b227259c84ac
ppurs <paulinaaa523@gmail.com>
2023-11-28 20:11:53 +0100

    fix: disable another user's note update by admin from panel

--- django_project/notes/admin.py
+++ django_project/notes/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from notes.models import Note
 
-admin.site.register(Note)
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, obj=None):  
+        if obj:
+            if request.user == obj.created_by:
+                return True
+            
+        return False
+
