691a9173105d94059bb2efa0f8b510e2a8ce2b93
ppurs <paulinaaa523@gmail.com>
2023-11-28 20:20:02 +0100

    fix: disable another user's topic update by admin from panel

--- django_project/topics/admin.py
+++ django_project/topics/admin.py
@@ -7,4 +7,11 @@ class TopicAdmin(MPTTModelAdmin):
     list_display = ['title' ,'public']
     list_editable = ['public']
 
+    def has_change_permission(self, request, obj=None):  
+        if obj:
+            if request.user == obj.created_by:
+                return True
+            
+        return False
+
 
